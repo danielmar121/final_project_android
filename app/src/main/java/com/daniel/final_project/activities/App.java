2c9b95c317d7f3c5c824deb0bfc217b0ae71c044
danielmar121 <danielmar121@gmail.com>
2020-12-15 19:49:38 +0200

    Done login page view!

---
+++ app/src/main/java/com/daniel/final_project/activities/App.java
@@ -0,0 +1,12 @@
+package com.daniel.final_project.activities;
+
+import android.app.Application;
+
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
