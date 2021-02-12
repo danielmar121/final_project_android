1eba91326e3c536680cea369a4116e4d0f091b79
Daniel <53561824+danielmar121@users.noreply.github.com>
2021-02-12 10:52:25 +0200

    Try hf recycler view (#10)
    * Change RecyclerView to HFRecyclerView
    
    * Change names
    
    * Change names

--- app/src/main/java/com/daniel/final_project/activities/buyer/ui/shops/AdapterShop.java
+++ app/src/main/java/com/daniel/final_project/activities/buyer/ui/shops/AdapterShop.java
@@ -1,70 +1,55 @@
 package com.daniel.final_project.activities.buyer.ui.shops;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
 import com.bumptech.glide.Glide;
 import com.daniel.final_project.R;
 import com.daniel.final_project.objects.Shop;
+import com.google.android.material.textfield.TextInputEditText;
+import com.mikhaellopez.hfrecyclerviewkotlin.HFRecyclerView;
 
-import java.util.List;
-
-public class AdapterShop extends RecyclerView.Adapter<AdapterShop.MyViewHolder> {
-    private List<Shop> shops;
-    private LayoutInflater mInflater;
+public class AdapterShop extends HFRecyclerView<Shop> {
     private MyItemClickListener mClickListener;
 
-    // data is passed into the constructor
-    AdapterShop(Context context, List<Shop> _shops) {
-        this.mInflater = LayoutInflater.from(context);
-        this.shops = _shops;
+    public AdapterShop() {
+        // With Header & With Footer
+        super(true, false);
     }
 
-    // inflates the row layout from xml when needed
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.shop_buyer_item, parent, false);
-        return new MyViewHolder(view);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder) {
+            ItemViewHolder itemViewHolder = (ItemViewHolder) holder;
+            itemViewHolder.bind(getItem(position));
+        } else if (holder instanceof HeaderViewHolder) {
+
+        } else if (holder instanceof FooterViewHolder) {
+
+        }
     }
 
-    // binds the data to the TextView in each row
+    //region Override Get ViewHolder
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        Shop shop = shops.get(position);
-        holder.shop_buyer_LBL_name.setText(shop.getName());
-        holder.movie_LBL_description.setText(shop.getDescription());
-        Glide
-                .with(mInflater.getContext())
-                .load(shop.getImageUrl())
-                .centerCrop()
-                .into(holder.shop_buyer_IMG_cover);
-
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mClickListener != null) {
-                    mClickListener.onItemClick(view, shop);
-                }
-            }
-        });
+    protected RecyclerView.ViewHolder getItemView(LayoutInflater inflater, ViewGroup parent) {
+        return new ItemViewHolder(inflater.inflate(R.layout.shop_buyer_item, parent, false));
     }
 
-    // total number of rows
     @Override
-    public int getItemCount() {
-        return shops.size();
+    protected RecyclerView.ViewHolder getHeaderView(LayoutInflater inflater, ViewGroup parent) {
+        return new HeaderViewHolder(inflater.inflate(R.layout.shops_buyer_header, parent, false));
     }
 
-    // convenience method for getting data at click position
-    public Shop getItem(int id) {
-        return shops.get(id);
+    @Override
+    protected RecyclerView.ViewHolder getFooterView(LayoutInflater inflater, ViewGroup parent) {
+        return null;
+        // return new FooterViewHolder(inflater.inflate(R.layout.item_footer, parent, false));
     }
 
     // allows clicks events to be caught
@@ -77,17 +62,52 @@ public class AdapterShop extends RecyclerView.Adapter<AdapterShop.MyViewHolder> 
         void onItemClick(View view, Shop shop);
     }
 
-    public class MyViewHolder extends ViewHolder {
+    //region ViewHolder Header and Footer
+    class ItemViewHolder extends RecyclerView.ViewHolder {
         TextView shop_buyer_LBL_name, movie_LBL_description;
         ImageView shop_buyer_IMG_cover;
 
-        MyViewHolder(View itemView) {
+        ItemViewHolder(View itemView) {
             super(itemView);
             shop_buyer_LBL_name = itemView.findViewById(R.id.shop_buyer_LBL_name);
             movie_LBL_description = itemView.findViewById(R.id.movie_LBL_description);
             shop_buyer_IMG_cover = itemView.findViewById(R.id.shop_buyer_IMG_cover);
+        }
 
+        void bind(Shop shop) {
+            shop_buyer_LBL_name.setText(shop.getName());
+            movie_LBL_description.setText(shop.getDescription());
+            Glide
+                    .with(itemView.getContext())
+                    .load(shop.getImageUrl())
+                    .centerCrop()
+                    .into(shop_buyer_IMG_cover);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mClickListener != null) {
+                        mClickListener.onItemClick(view, shop);
+                    }
+                }
+            });
         }
     }
+
+    class HeaderViewHolder extends RecyclerView.ViewHolder {
+        TextInputEditText buyer_shops_ETXT_search;
+        ImageView buyer_shops_IMG_advertise;
+
+        HeaderViewHolder(View view) {
+            super(view);
+        }
+    }
+
+    class FooterViewHolder extends RecyclerView.ViewHolder {
+        FooterViewHolder(View view) {
+            super(view);
+        }
+    }
+    //endregion
 
 }
--- app/src/main/java/com/daniel/final_project/activities/buyer/ui/shops/ShopsFragment.java
+++ app/src/main/java/com/daniel/final_project/activities/buyer/ui/shops/ShopsFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,7 +15,6 @@ import com.daniel.final_project.R;
 import com.daniel.final_project.interfaces.buyer.BuyerShopsCallBack;
 import com.daniel.final_project.objects.Shop;
 import com.daniel.final_project.services.MyFireBase;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.gson.Gson;
 
 import java.util.List;
@@ -25,14 +23,12 @@ public class ShopsFragment extends Fragment {
 
     View root;
     MyFireBase myFireBase;
-    TextInputEditText buyer_shops_ETXT_search;
-    ImageView buyer_shops_IMG_advertise;
     RecyclerView buyer_shops_LST_shops;
 
     private BuyerShopsCallBack buyerShopsCallBack = new BuyerShopsCallBack() {
         @Override
         public void putShopsInList(List<Shop> shops) {
-            AdapterShop adapterShop = new AdapterShop(getContext(), shops);
+            AdapterShop adapterShop = new AdapterShop();
 
             adapterShop.setClickListener(new AdapterShop.MyItemClickListener() {
                 @Override
@@ -45,6 +41,7 @@ public class ShopsFragment extends Fragment {
                 }
             });
 
+            adapterShop.setData(shops);
             buyer_shops_LST_shops.setLayoutManager(new LinearLayoutManager(getContext()));
             buyer_shops_LST_shops.setAdapter(adapterShop);
         }
@@ -68,8 +65,6 @@ public class ShopsFragment extends Fragment {
     }
 
     private void findViews() {
-        buyer_shops_ETXT_search = root.findViewById(R.id.buyer_shops_ETXT_search);
-        buyer_shops_IMG_advertise = root.findViewById(R.id.buyer_shops_IMG_advertise);
         buyer_shops_LST_shops = root.findViewById(R.id.buyer_shops_LST_shops);
     }
 }
